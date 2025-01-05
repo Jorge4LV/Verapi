1e013d5240b974d29dc5b684e6cd44cd7884c698
MethDev ALV <47840499+Jorge4LV@users.noreply.github.com>
2025-01-05 16:57:53 -0600

    Comeback again
    Resulta que si estoy más pendejo de lo que creía xd

--- frontend/next.config.ts
+++
@@ -1,7 +0,0 @@
-import { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "standalone", // Necesario para trabajar en Vercel con múltiples builds
-};
-
-export default nextConfig;
